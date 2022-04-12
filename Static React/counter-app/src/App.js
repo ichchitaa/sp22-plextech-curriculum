470197fff61c57fc3dae2ae4b923cab546223f3e
Ichchitaa Sawrikar <ichchitaa@gmail.com>
2022-04-11 18:13:56 -0700

    fixed bug in React Intro Project

--- Static React/counter-app/src/App.js
+++ Static React/counter-app/src/App.js
@@ -1,29 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import Button from './Button.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
 
 export default class App extends Component {
   constructor(){
@@ -52,12 +31,12 @@ export default class App extends Component {
       <div>
         <h2>Count: { count } </h2>
         <Button
-          title = { "+" }
-          task = { () => this.increment() }
+          name = { "+" }
+          todo = { () => this.increment() }
         />
         <Button
-          title = { "-" }
-          task = { () => this.decrement() }
+          name = { "-" }
+          todo = { () => this.decrement() }
         />
        </div>
     );
